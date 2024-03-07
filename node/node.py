3d2c369a4e92390bde083d94858eb95d23b3cd4d
bitcoinski <superrareuser@protonmail.com>
2024-03-07 12:25:55 -0500

    Update node.py

--- node/node.py
+++ node/node.py
@@ -46,24 +46,67 @@ class LoopEnd:
         return ()
 
 
+class LoopStart_SEGS:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"first_loop": s.RETURN_TYPES, "loop": ("LOOP",), "image": ("IMAGE",)}}
+
+    RETURN_TYPES = ()
+    FUNCTION = "run"
+    CATEGORY = "loopback"
+
+    def run(self, first_loop, loop):
+        if hasattr(loop, 'next'):
+            return (loop.next,)
+        return (first_loop,)
+
+    @classmethod
+    def IS_CHANGED(s, first_loop, loop):
+        if hasattr(loop, 'next'):
+            return id(loop.next)
+        return float("NaN")
+
+class LoopEnd_SEGS:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "send_to_next_loop": "IMAGE", "loop": ("LOOP",) }}
+
+    RETURN_TYPES = ()
+    LOOP_TYPE = ()
+    FUNCTION = "run"
+    CATEGORY = "loopback"
+    OUTPUT_NODE = True
+
+    def run(self, send_to_next_loop, loop):
+        loop.next = send_to_next_loop
+        return ()
+
 
 NODE_CLASS_MAPPINGS = {
     "Loop": Loop,
     "LoopStart": LoopStart,
-    "LoopEnd": LoopEnd
+    "LoopEnd": LoopEnd,
+    "LoopStart_SEGS": LoopStart_SEGS,
+    "LoopEnd_SEGS": LoopEnd_SEGS
 }
 
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoopEnd": "LoopEnd",
     "LoopStart": "LoopStart",
-    "LoopEnd": "LoopEnd"
+    "LoopEnd": "LoopEnd",
+    "LoopStart_SEGS": "LoopStart_SEGS",
+    "LoopEnd_SEGS": "LoopEnd_SEGS"
 }
 
 def addLoopType(t):
     NODE_CLASS_MAPPINGS["LoopStart_" + t] = type("LoopStart_" + t, (LoopStart, ), { "RETURN_TYPES": (t,) })
     NODE_CLASS_MAPPINGS["LoopEnd_" + t] = type("LoopEnd_" + t, (LoopEnd, ), { "LOOP_TYPE": (t,) })
+    NODE_DISPLAY_NAME_MAPPINGS["LoopStart_" + t] = ["LoopStart_" + t]
     NODE_DISPLAY_NAME_MAPPINGS["LoopEnd_" + t] = ["LoopEnd_" + t]
+
+
+#SEGS
 
 # ComfyUI types
 addLoopType("IMAGE")
@@ -72,7 +115,7 @@ addLoopType("LATENT")
 addLoopType("MASK")
 addLoopType("MODEL")
 
-addLoopType("SEGS")
+#addLoopType("SEGS")
 
 # These don't work for some reason :/
 # addLoopType("FLOAT")
